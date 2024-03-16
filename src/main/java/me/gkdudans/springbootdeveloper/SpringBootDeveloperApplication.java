661597680c8713df2a2d9a246b8b068c476a4b63
gkdudans <chrismhy001@gmail.com>
2024-03-17 02:45:39 +0900

    00장 개발 환경 구축하기

---
+++ src/main/java/me/gkdudans/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,8 @@
+package me.gkdudans.springbootdeveloper;
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
