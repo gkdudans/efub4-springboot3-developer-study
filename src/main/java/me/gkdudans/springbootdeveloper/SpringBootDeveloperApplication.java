c77a7c8f8869ed2b39d048019098f590b2806d7d
gkdudans <chrismhy001@gmail.com>
2024-03-17 05:51:10 +0900

    02장 스프링 부트 3 시작하기

--- src/main/java/me/gkdudans/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/gkdudans/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -1,6 +1,9 @@
 package me.gkdudans.springbootdeveloper;
-import org.springframework.boot.SpringApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args){
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
