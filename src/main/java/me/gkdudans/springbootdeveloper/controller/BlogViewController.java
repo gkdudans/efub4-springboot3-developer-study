b17349c27d99524e661c2e8ce74fefc75f4c7a07
gkdudans <chrismhy001@gmail.com>
2024-05-08 23:26:53 +0900

    08장 스프링 시큐리티로 로그인/로그아웃, 회원가입 구현하기

---
+++ src/main/java/me/gkdudans/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,54 @@
+package me.gkdudans.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.gkdudans.springbootdeveloper.service.BlogService;
+import me.gkdudans.springbootdeveloper.domain.Article;
+import me.gkdudans.springbootdeveloper.dto.ArticleListViewResponse;
+import me.gkdudans.springbootdeveloper.dto.ArticleViewResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+
+    /* 블로그 글 뷰 구현 */
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+    /* 수정 구현 */
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
+    }
+}
